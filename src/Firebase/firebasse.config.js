b39fe6596e476beb6a94a9dcafe59c80ad1551ba
shajjad00 <shajjadph@gmail.com>
2023-11-13 03:03:38 +0600

    added environmet variable

--- src/Firebase/firebasse.config.js
+++ src/Firebase/firebasse.config.js
@@ -1,22 +1,14 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 
-// Your web app's Firebase configuration
-
-console.log(import.meta.env.FIREBASE_AUTH_apiKey);
+console.log(import.meta.env.VITE_APIKEY);
 const firebaseConfig = {
-  apiKey: "AIzaSyCv3CMWtUDuSiDrfSfsfyXgc2EFhiczrEM",
-  authDomain: "password-auth-d7366.firebaseapp.com",
-  projectId: "password-auth-d7366",
-  storageBucket: "password-auth-d7366.appspot.com",
-  messagingSenderId: "627909301105",
-  appId: "1:627909301105:web:c14c2f603082103cd29cd0",
-  //   apiKey: process.env.FIREBASE_AUTH_apiKey,
-  //   authDomain: process.env.FIREBASE_AUTH_authDomain,
-  //   projectId: process.env.FIREBASE_AUTH_projectId,
-  //   storageBucket: process.env.FIREBASE_AUTH_storageBucket,
-  //   messagingSenderId: process.env.FIREBASE_AUTH_messagingSenderId,
-  //   appId: process.env.FIREBASE_AUTH_appId,
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
